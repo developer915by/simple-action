6eec69373c0ecd8f004ca7a878ecfe3c07aebb08
developer915by <30619913+developer915by@users.noreply.github.com>
2018-05-22 14:05:45 +0300

    begin action
    simple action, mechanics of shots, taking damage

---
+++ Assets/Assets/scripst/SettingsPopup.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SettingsPopup : MonoBehaviour {
+	[SerializeField] private Slider speedSlider;
+	void Start()
+	{
+		speedSlider.value = PlayerPrefs.GetFloat ("speed", 1);
+	}
+	public void OnSubmitName(string name)  // Этот метод срабатывает в момент начала ввода данных в текстовом поле
+	{
+		Debug.Log (name);
+	}
+	public void OnSpeedValue(float speed) // Этот метод срабатывает при изменении положения ползунка
+	{
+		Messenger<float>.Broadcast (GameEvent.SPEED_CHANGED, speed); // значение, заданное положением ползунка, рассылается как событие <float>
+		//Debug.Log ("Speed: " + speed);
+	}
+	public void Open()
+	{
+		gameObject.SetActive (true); //активируете этот объект, чтобы открыть окно
+	}
+	public void Close()
+	{
+		gameObject.SetActive (false); //деативируете обЪект, чтобы закрыть окно
+
+	}
+}
---
+++ Assets/Assets/scripst/UIController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // Импорт инфраструктуры для работы с кодом UI 
+
+
+public class UIController : MonoBehaviour {
+	[SerializeField] private Text scoreLabel; // Обьект сцены Reference Text, предназначеный для создания свойства text
+	[SerializeField] private SettingsPopup settingsPopup;
+
+
+	/*void Update () {
+		scoreLabel.text = Time.realtimeSinceStartup.ToString (); отображалось время игры вместо счёта, тестовый кусок кода
+	}*/ 
+	public void OnPointerDown () 
+	{
+		Debug.Log ("pointer down");
+	}
+	public void OnOpenSettings() { // метод вызываемый кнопкой настроек 
+		settingsPopup.Open(); // Заменяем отладочный текст методом всплывающего окна	Debug.Log ("open settings");
+	}
+	private int _score;
+
+	void Awake()
+	{
+		Messenger.AddListener (GameEvent.ENEMY_HIT, OnEnemyHit); // Объявляем, какой метод отвечает на событие ENEMY_HIT
+	}
+	void OnDestroy()
+	{
+		Messenger.RemoveListener (GameEvent.ENEMY_HIT, OnEnemyHit); // При разрушении объекта удаляйте подписчика, чтобы избежать ошибок.
+	}
+		
+	void Start()
+	{
+		_score = 0;
+		scoreLabel.text = _score.ToString (); // Присвоение переменной score начального значения 0.
+		settingsPopup.Close ();
+	}
+	private void OnEnemyHit()
+	{
+		_score += 1; // Увеличение переменной score на 1 в ответ на данное событие.
+		scoreLabel.text = _score.ToString();
+	}
+}
